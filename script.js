e4603c27a4dddb0ce571b601498b0d7953a4a95a
Manveer <155811338+Manveer04@users.noreply.github.com>
2026-07-21 19:17:02 +0800

    4rd commit

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
   // CONFIGURATION
   // ========================================
 
-  const API_URL = "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL";
+  const API_URL = "https://script.google.com/macros/s/AKfycbxF4XP8dbiJwv6D5U3mG_9woUAJrUVIytDYhMcqPad1LV99d2U_GUJWF0tR3rWHvNHB/exec";
   const REFRESH_INTERVAL = 30000;
 
   const CONTACTS = [
